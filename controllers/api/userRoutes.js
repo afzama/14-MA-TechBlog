03be6d02d246a9383aa7a70f9d43f6324605ee90
afzama <141961264+afzama@users.noreply.github.com>
2024-02-04 22:25:37 -0500

    login route updates

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const { User, Blogpost, Comment } = require('../../models');
+const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 //User login route
